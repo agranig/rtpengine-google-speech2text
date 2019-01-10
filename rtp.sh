e9aada195a812e5e97f975c7d87576608fd4e2f1
Andreas Granig <agranig@linguin.org>
2019-01-10 12:46:09 +0100

    Fix script to stop recording daemon
    Our own py script is taking the role of the recording daemon instead

--- rtp.sh
+++ rtp.sh
@@ -2,5 +2,5 @@
 
 export GOOGLE_APPLICATION_CREDENTIALS=google-service-account.json
 rm -f /tmp/out*.wav
-/etc/init.d/ngcp-rtpengine-recording-daemon start 1>/dev/null 2>/dev/null
+/etc/init.d/ngcp-rtpengine-recording-daemon stop 1>/dev/null 2>/dev/null
 ./rtp.py | grep '>>>'
